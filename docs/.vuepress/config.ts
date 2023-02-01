c052efd8f38d01bc32c5615b3bed492cfec04547
zhutongtong <zhutongtong@360.cn>
2023-02-01 13:23:04 +0800

    修改base

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,7 @@
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
-  base: '/vuepress-blog/',
+  // base: '/vuepress-blog/',
   lang: 'zh-CN',
   title: '你1好， VuePress ！',
   description: '这是我的第一个 VuePress 站点',
